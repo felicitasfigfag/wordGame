28b722508dddf8558d85ea6a00b27a3dc8c3af55
Felicitas <felifigueroaf@gmail.com>
2023-08-17 11:36:43 +0200

    Unshown indices resets after hitting limit

--- wordGame/wordGame/ViewModels/WordViewModel.swift
+++ wordGame/wordGame/ViewModels/WordViewModel.swift
@@ -9,16 +9,16 @@ import Foundation
 
 class WordViewModel {
     private var wordService: WordService
-    var wordPairs: [WordPair] = []{
-        didSet{
-            print("Word pairs are: ", self.wordPairs.count)
-        }
-    }
+    var wordPairs: [WordPair] = []
     var unshownIndices: [Int] = []
     
     init(service: WordService) {
         self.wordService = service
         loadWords()
+        setUnshownIndices()
+    }
+    
+    func setUnshownIndices(){
         self.unshownIndices = Array(0..<wordPairs.count)
     }
     
@@ -29,10 +29,10 @@ class WordViewModel {
         }
     }
     
-    
-
     func getRandomPair() -> WordPair? {
-        guard !unshownIndices.isEmpty else { return nil }
+        if unshownIndices.isEmpty {
+            setUnshownIndices()
+        }
 
         let randomPosition = Int.random(in: 0..<unshownIndices.count)
         let randomIndex = unshownIndices[randomPosition]
@@ -47,16 +47,14 @@ class WordViewModel {
             selectedPair.correct = true
             return selectedPair
         } else {
-            // Choose a random Spanish word for incorrect pairing
-            let incorrectSpanishIndex = Int.random(in: 0..<wordPairs.count)
-            selectedPair.translation = wordPairs[incorrectSpanishIndex].translation
+            // Choose a random translated word for incorrect pairing
+            let incorrectTranslatedIndex = Int.random(in: 0..<wordPairs.count)
+            selectedPair.translation = wordPairs[incorrectTranslatedIndex].translation
             selectedPair.correct = false
             return selectedPair
         }
     }
 
-
-    
     
 }
 
